75afa95eff67be7d88be2fd7f6c1a97d4c733f20
rezuan <rezuan.ahmed.pro@gmail.com>
2024-09-26 21:45:50 +0600

    styles: styled components setup and global stylesimplement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: #fff;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wild Oasis</H1>
+        <Input placeholder="Number of guests" />
+        <Button>Check in</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
