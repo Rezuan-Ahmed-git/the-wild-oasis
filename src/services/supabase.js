254c55877346696eef28201bb9078a287d52cd74
rezuan <rezuan.ahmed.pro@gmail.com>
2024-10-02 17:22:21 +0600

    feat: supabase connected

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://urrryjvtnxadtpudfbcy.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycnJ5anZ0bnhhZHRwdWRmYmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4NjA1MjQsImV4cCI6MjA0MzQzNjUyNH0.0Hw5LNlUCePh-WMbRrqcOPkbIgOSNdW-EHUlYf51WZ4';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
