2804d6f5709d89e6f190ed026bffa0d72716a8a4
rezuan <rezuan.ahmed.pro@gmail.com>
2024-10-07 20:49:57 +0600

    feat: create cabin functionality implement

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,20 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newValue) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([newValue])
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
