1c83bf87fb9cb1e77ce2738e27b066ff54b0f503
rezuan <rezuan.ahmed.pro@gmail.com>
2024-10-29 16:55:35 +0600

    fix: remove some unused variables

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('rezuanahmed.dev@gmail.com');
-  const [password, setPassword] = useState('1234');
+  const [email, setEmail] = useState('ridwanshourav@gmail.com');
+  const [password, setPassword] = useState('87654321');
   const { isLoading, login } = useLogin();
 
   function handleSubmit(e) {
